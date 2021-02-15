6f23349b42e91919660576c9079f046164da79d4
fusebox123 <joelphilipjacob@gmail.com>
2021-02-15 21:10:40 +0530

    exercise SpringBoot CRUD

---
+++ exercisetoolapi/src/main/java/com/cg/healthify/ExercisetoolapiApplication.java
@@ -0,0 +1,13 @@
+package com.cg.healthify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExercisetoolapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExercisetoolapiApplication.class, args);
+	}
+
+}
